7f1249d206a0ffac4aa3cdfd48c3c9caba34f17e
鹿取 祐二 <94896186+yuji-katori@users.noreply.github.com>
2026-02-23 15:04:55 +0900

    Add files via upload

---
+++ pointer/sample/hwsetup.c
@@ -0,0 +1,48 @@
+/*
+* Copyright (c) 2013(2025) Renesas Electronics Corporation and/or its affiliates
+*
+* SPDX-License-Identifier: BSD-3-Clause
+*/
+/************************************************************************
+*
+* Device     : RX/RX100/RX110
+*
+* File Name  : hwsetup.c
+*
+* Abstract   : Hardware Setup file.
+*
+* History    : 1.00  (2013-07-23)  [Hardware Manual Revision : 0.50]
+*            : 1.00A (2025-04-11)  [Hardware Manual Revision : 1.00]
+*
+* NOTE       : THIS IS A TYPICAL EXAMPLE.
+*
+************************************************************************/
+
+#include "iodefine.h"
+#ifdef __cplusplus
+extern "C" {
+#endif
+extern void HardwareSetup(void);
+#ifdef __cplusplus
+}
+#endif
+
+void HardwareSetup(void)
+{
+/*
+ BSC.CS0MOD.WORD = 0x1234;
+ BSC.CS7CNT.WORD = 0x5678;
+  
+ SCI0.SCR.BIT.TE  = 0;
+ SCI0.SCR.BIT.RE  = 0;
+ SCI0.SCR.BIT.TE  = 1;
+ SCI2.SSR.BIT.PER = 0;
+
+ TMR0.TCR.BYTE = 0x12;
+ TMR1.TCR.BYTE = 0x12;
+ TMR2.TCR.BYTE = 0x12;
+ 
+ P0.DDR.BYTE = 0x12;
+ P1.DDR.BYTE = 0x12;
+*/
+}
---
+++ pointer/sample/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main(void)
+{
+int a;
+
+	printf("Input : ");
+	scanf("%d", &a );
+	printf("Output: %d",a );
+
+	__nop( );
+}
